7f3182fbbe8f4a3331bae30151d53618212a8369
fanoll <fabian.noll@karlstorz.com>
2022-12-03 11:08:29 +0100

    did part 1 and 2 of AOC Day One

---
+++ AOCDayOne/CalorieCounter.cs
@@ -0,0 +1,51 @@
+﻿namespace AOCDayOne;
+
+public class CalorieCounter
+{
+    public int mostCalories = 0;
+    public int secondMostCalories = 0;
+    public int thirdMostCalories = 0;
+    public int currentCalories = 0;
+    public int CountCalories()
+    {
+        foreach (string line in System.IO.File.ReadLines(@"./elves_inventory.txt"))
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                if (currentCalories > mostCalories)
+                {
+                    int temp = mostCalories;
+                    mostCalories = currentCalories;
+                    currentCalories = secondMostCalories;
+                    secondMostCalories = temp;
+                    thirdMostCalories = currentCalories;
+                    currentCalories = 0;
+                    continue;
+                    
+                } else if (currentCalories > secondMostCalories)
+                {
+                    int temp = secondMostCalories;
+                    secondMostCalories = currentCalories;
+                    thirdMostCalories = temp;
+                    currentCalories = 0;
+                    continue;
+                    
+                } else if (currentCalories > thirdMostCalories)
+                {
+                    thirdMostCalories = currentCalories;
+                    currentCalories = 0;
+                    continue;
+                }
+                else
+                {
+                    currentCalories = 0;
+                    continue;
+                }
+            }
+
+            int calories = Int32.Parse(line);
+            currentCalories += calories;
+        }
+        return mostCalories + secondMostCalories + thirdMostCalories;
+    }
+}
---
+++ AOCDayOne/Program.cs
@@ -0,0 +1,5 @@
+﻿
+using AOCDayOne;
+
+var _counter = new CalorieCounter();
+Console.WriteLine(_counter.CountCalories());

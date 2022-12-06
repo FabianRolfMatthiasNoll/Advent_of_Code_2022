9ab975f77d32fecddeba3110acd2a713cf330d15
fanoll <fabian.noll@karlstorz.com>
2022-12-06 09:51:03 +0100

    added to solve part two of day 3. No changes to program of part one because of the different angle of solving the problem

---
+++ AOCDay3/BadgeFinder.cs
@@ -0,0 +1,60 @@
+﻿namespace AOCDay3;
+
+public class BadgeFinder
+{
+    public int FindBadgeSum()
+    {
+        var count = 1;
+        var itemSum = 0;
+        var itemFound = false;
+        char[] backpackOne = {};
+        char[] backpackTwo = {};
+        char[] backpackThree = {};
+        
+        foreach (string line in System.IO.File.ReadLines(@"./backpack_inventory.txt"))
+        {
+            if (count == 1)
+            {
+                backpackOne = line.ToCharArray();
+                count++;
+            } else if (count == 2)
+            {
+                backpackTwo = line.ToCharArray();
+                count++;
+            } else if (count == 3)
+            {
+                backpackThree = line.ToCharArray();
+                foreach (var itemOne in backpackOne)
+                {
+                    foreach (var itemTwo in backpackTwo)
+                    {
+                        foreach (var itemThree in backpackThree)
+                        {
+                            if (itemOne == itemTwo && itemTwo == itemThree)
+                            {
+                                if (char.IsUpper(itemOne))
+                                {
+                                    itemSum += ((int)itemOne % 32) + 26;
+                                    itemFound = true;
+                                    count = 1;
+                                }
+                                else
+                                {
+                                    itemSum += (int)itemOne % 32;
+                                    itemFound = true;
+                                    count = 1;
+                                }
+                                break;
+                            }
+                        }
+                        if (itemFound) break;
+                    }
+                    if (itemFound) break;
+                }
+                itemFound = false;
+            }
+
+        }
+        return itemSum;
+    }
+}
